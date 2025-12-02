0ca11d23593e7db12e633a2dcac15216dc6aadd7
Rogers118 <150867362+Rogers118@users.noreply.github.com>
2025-12-02 19:16:54 +0300

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -12,6 +12,7 @@ const auth = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
+        next();
     } catch (err) {
         return res.status(401).json({ msg: 'Invalid or expired token' });
     }
